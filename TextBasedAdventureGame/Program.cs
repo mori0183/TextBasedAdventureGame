e6bca69bc5f555acd40fde463eed28453bfe1e6c
mori0183 <mori0183@algonquinlive.com>
2013-11-27 18:06:44 -0500

    Added some SHIIIEEET Cracker ass hate monger!

--- TextBasedAdventureGame/Program.cs
+++ TextBasedAdventureGame/Program.cs
@@ -6,8 +6,7 @@ using System.Text;
 namespace TextBasedAdventureGame
 {
     class Program
-    {
-        
+    {        
         static void Main(string[] args)
         {
             bool playing = true;
@@ -22,21 +21,25 @@ namespace TextBasedAdventureGame
 
     public class Game
     {
+        bool atMill = false;
         bool beenHereWater = false;
         bool lookedAround = false;
         List<string> inventory = new List<string>();
 
         public void reset()
         {
-            inventory.Add("EndOfInv");
+            inventory.Add("EndOfInventory");
 
             beenHereWater = false;
             lookedAround = false;
+            StartGame();
         }
 
+        //EVERY action revolves from this statement
         public void checkIfValid(string toCheck)
         {
             whereTo(toCheck);
+
         }
 
         private void whereTo(string toHere)
@@ -44,19 +47,37 @@ namespace TextBasedAdventureGame
             switch (toHere)
             {
                 case "Water":
+                case "water":
                     waterStuff();
                     break;
 
                 case "Look around":
+                case "look around":
+                case "Look Around":
+                case "look Around":
                     LookAround();
                     break;
 
                 case "Test Inventory":
+                case "test inventory":
+                case "Test inventory":
+                case "test Inventory":
                     testInventory();
                     break;
 
                 case "Inventory":
+                case "inventory":
                     printInventory();
+                    break;
+
+                case "Field":
+                case "field":
+                    FieldOfShit();
+                    break;
+
+                case "River":
+                case "river":
+                    River();
                     break;
             }
         }
@@ -71,6 +92,7 @@ namespace TextBasedAdventureGame
 
         private void waterStuff()
         {
+            Console.Beep();
             foreach(string inventoryItem in inventory)
             {
                 if (inventoryItem == "Rope")
@@ -141,6 +163,7 @@ namespace TextBasedAdventureGame
                 {
                     if (inventoryItem == null)
                     {
+                        
                         inventory.Add("Rope");
                         lookedAround = true;
                         return;
@@ -157,8 +180,43 @@ namespace TextBasedAdventureGame
 
             else
             {
-                Console.WriteLine("You've already looked around. There is nothing interesting anymore");
+                Console.WriteLine("You've already looked around.\n There is nothing interesting anymore");
             }
         }
+        private void StartGame()
+        {
+            Console.WriteLine("          Welcome to FAIL QUEST.\nThis text based adventure will test your failure awesomenessness.\n\nYou have just exited the town of Shmagma, ");
+            Console.WriteLine("it is a hot summer day with a nice cool wind blowing east and everything is\nperfect.");
+            Console.WriteLine("All of a sudden a shriek comes from the north east of your position.\n");
+            Console.WriteLine("Quickly you spin around and witness a massive DEMON carring a princess to the\ncastle behind the town you just left.");
+            Console.WriteLine("You feel you must do what you can to save the princess from her fate,\nbut you must aquire itams to aid you on your quest.\n");
+            Console.WriteLine("you are facing a path that leads to a beautiful field of tall grass.\n");
+            Console.WriteLine("Behind you is the town of Shmagma from which you have just exited.\n");
+            Console.WriteLine("Where do you choose to go?\n");
+        }
+
+        private void FieldOfShit()
+        {
+            Console.Clear();
+            Console.WriteLine("As you approach the field you notice a fairly recognizable smell");
+            Console.WriteLine("You realize it is the smell of feces, but you are unable to ");
+            Console.WriteLine("Identify the origins of what it came from.");
+            Console.WriteLine("To the east lies the RIVER,\nto the south, the MILL,\nand to the west is the FOREST.");
+            Console.WriteLine("\nWhere will you go?");
+            Console.WriteLine("The RIVER.");
+            Console.WriteLine("The MILL.");
+            Console.WriteLine("The FOREST.\n");
+        }
+
+        private void River()
+        {
+            Console.Clear();
+
+            Console.WriteLine("You approach the river with caution as the rapids could easily pull you");
+            Console.WriteLine(" in and make short work of you. A ROPE could be quite handy right now");
+
+            //Add an if statement that checks for rope, if rope is in inventory you may cross the river
+        }
+
     }
 }
